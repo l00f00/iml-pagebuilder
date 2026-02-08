7858ade7b964edbe8801fbefa18f5ae478e5dd7e
LUCA BARTOLO <luca.bartolo@trusttis.com>
2026-02-08 08:53:14 +0100

    Optimize image sizes: use 'large' for lightbox and featured images to improve loading speed

--- includes/shortcode-project-single.php
+++ includes/shortcode-project-single.php
@@ -22,9 +22,9 @@ function iml_render_project_single($atts) {
     // Retrieve the post thumbnail
     $thumbnail_id = get_post_thumbnail_id($post_id); 
     $has_single_page = get_post_meta($thumbnail_id, 'has_single_page', true); 
-    $thumbnail_url = $has_single_page ? get_permalink($thumbnail_id) : wp_get_attachment_image_url($thumbnail_id, 'full'); 
+    $thumbnail_url = $has_single_page ? get_permalink($thumbnail_id) : wp_get_attachment_image_url($thumbnail_id, 'large'); 
     $lightbox_attr = $has_single_page ? '' : 'data-lightbox="gallery"'; 
-    $featured_image_url = wp_get_attachment_image_url($thumbnail_id, 'full'); 
+    $featured_image_url = wp_get_attachment_image_url($thumbnail_id, 'large'); 
     
     // Conditional check for space and layout
     $space = rwmb_meta( 'abilitaSpazio' ); 
@@ -53,7 +53,7 @@ function iml_render_project_single($atts) {
     <div class="progetto-content"> 
         <div class="left-column-progetto"> 
           <a href="<?php echo esc_url($featured_image_url); ?>" style="color:black;" data-lightbox="gallery"> 
-            <?php echo get_the_post_thumbnail($post_id, 'full'); ?></a> 
+            <?php echo get_the_post_thumbnail($post_id, 'large'); ?></a> 
           <div class="left-column-bottom"> 
                 <nav class="foto-navigation"> 
                         <?php 
@@ -92,7 +92,7 @@ function iml_render_project_single($atts) {
             // Check if the item should link to a single page 
             $single_page_true = get_post_meta($foto_id, 'has_single_page', true); 
             // Get the full-size image URL and the large thumbnail URL 
-            $image_url = wp_get_attachment_image_url($foto_id, 'full'); // Reverted to full size for better quality
+            $image_url = wp_get_attachment_image_url($foto_id, 'large'); // Changed from full URL to large size
             $thumbnail = wp_get_attachment_image_url($foto_id, 'large'); 
             // Determine the link URL and whether to use lightbox 
             //$link_url = $single_page_true ? get_permalink($foto_id) : esc_url($image_url); 
@@ -327,7 +327,7 @@ function iml_render_project_single($atts) {
     <div class="progetto-content"> 
         <div class="left-column-progetto"> 
           <a href="<?php echo esc_url($featured_image_url); ?>" style="color:black;" data-lightbox="gallery"> 
-            <?php echo get_the_post_thumbnail($post_id, 'full'); ?></a> 
+            <?php echo get_the_post_thumbnail($post_id, 'large'); ?></a> 
           <div class="left-column-bottom"> 
                 <nav class="foto-navigation"> 
                         <?php 
@@ -367,7 +367,7 @@ function iml_render_project_single($atts) {
             // Check if the item should link to a single page 
             $single_page_true = get_post_meta($foto_id, 'has_single_page', true); 
             // Get the full-size image URL and the large thumbnail URL 
-            $image_url = wp_get_attachment_image_url($foto_id, 'full'); // Reverted to full size for better quality
+            $image_url = wp_get_attachment_image_url($foto_id, 'large'); // Changed from full URL to large size
             $thumbnail = wp_get_attachment_image_url($foto_id, 'large'); 
             // Determine the link URL and whether to use lightbox 
             //$link_url = $single_page_true ? get_permalink($foto_id) : esc_url($image_url); 
