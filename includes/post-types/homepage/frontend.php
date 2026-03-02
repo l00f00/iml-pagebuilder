d732ea5a5716258486db737c67439d513be1b66d
LUCA BARTOLO <luca.bartolo@trusttis.com>
2026-03-02 11:50:47 +0100

    fixdebug

--- includes/post-types/homepage/frontend.php
+++ includes/post-types/homepage/frontend.php
@@ -255,7 +255,7 @@ function iml_homepage_lottie_preloader() {
         // Imposta a true per attivare il riposizionamento forzato dei layer Lottie
         // affinché coincidano con gli elementi statici HTML.
         var enableSync = true; 
-        var debugSync = true; // ATTIVA DEBUG VISUALE (Richiesta Utente)
+        var debugSync = false; // ATTIVA DEBUG VISUALE (Richiesta Utente)
 
         // Mappa di corrispondenza: NOME LAYER LOTTIE => SELETTORE HTML STATICO
         // Assicurati che il JSON Lottie sia stato esportato con l'opzione "Include Layer Names" (Bodymovin/LottieFiles)
